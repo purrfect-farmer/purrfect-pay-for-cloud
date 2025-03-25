84a485e002270fe3dac0cc86897871207b075a47
Sadiq Salau <sadiqsalau888@gmail.com>
2025-03-25 04:01:42 +0100

    Added Entries

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        iframe: resolve(__dirname, "iframe.html"),
+        redirect: resolve(__dirname, "redirect.html"),
+      },
+    },
+  },
   plugins: [tailwindcss()],
 });
